a9b2dffa4f064ea4e7506b0f4fe21eff8cc87ab4
Adrián Camacho <a.camacho.2023@alumnos.urjc.es>
2025-02-21 12:41:03 +0100

    AÑADIDO PRIZES, faltan muchos cambios ahi

--- src/main/java/com/casino/grupo4_dws/casinoweb/model/Bet.java
+++ src/main/java/com/casino/grupo4_dws/casinoweb/model/Bet.java
@@ -1,20 +1,40 @@
 package com.casino.grupo4_dws.casinoweb.model;
 
 public class Bet {
-    private int input;
-    private int output;
+    private int amount;
+    private int revenue = 0;
+    private String user;
+    private int date;
+    private boolean status;
+    private int gameID;
 
-    public int getInput() {
-        return input;
+
+    public int GetAmount() {
+        return amount;
     }
 
-    public void setInput(int amount) {
-        this.input = amount;
+    public void setAmount(int amount) {
+        this.amount = amount;
     }
-    public int getOutput() {
-        return output;
-    }
-    public void setOutput(int amount) {
-        this.output = amount;
-    }
+
+    public int GetRevenue() { return revenue;}
+
+    public void setRevenue(int revenue) {this.revenue = revenue;}
+
+    public String GetUser() {return user;}
+
+    public void setUser(String user) {this.user = user;}
+
+    public int GetDate() {return date;}
+
+    public void setDate(int date) {this.date = date;}
+
+    public boolean GetStatus() {return status;}
+
+    public void setStatus(boolean status) {this.status = status;}
+
+    public int GetGameID() {return gameID;}
+
+    public void setGameID(int gameID) {this.gameID = gameID;}
+
 }
