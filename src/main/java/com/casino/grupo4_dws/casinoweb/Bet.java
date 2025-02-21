440d25fd71fdd62fde609c79d1bbccb56a1578f5
saultj <sauldelasgalaxias@gmail.com>
2025-02-21 12:01:28 +0100

    Añadida clase Bet

---
+++ src/main/java/com/casino/grupo4_dws/casinoweb/Bet.java
@@ -0,0 +1,13 @@
+package com.casino.grupo4_dws.casinoweb;
+
+public class Bet {
+    private int amount;
+
+    public int GetAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
---
+++ src/main/java/com/casino/grupo4_dws/casinoweb/Prize.java
@@ -0,0 +1,36 @@
+package com.casino.grupo4_dws.casinoweb;
+
+public class Prize {
+    private String title;
+    private int price;
+    private String description;
+    private String Image;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public void setImage(String image) {
+        Image = image;
+    }
+
+    public String GetTitle() {
+        return title;
+    }
+    public int GetPrice() {
+        return price;
+    }
+    public String GetDescription() {
+        return description;
+    }
+    public int GetImage() {
+        return Integer.parseInt(Image);
+    }
+}
+
+
