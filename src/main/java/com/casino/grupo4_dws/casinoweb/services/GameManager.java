40c7617f3f2ae1830863c87af0e126f56eff9fcc
saultj <sauldelasgalaxias@gmail.com>
2025-02-22 10:39:26 +0100

    Añadido Juego de dado + gestion de lista

--- src/main/java/com/casino/grupo4_dws/casinoweb/services/GameManager.java
+++ src/main/java/com/casino/grupo4_dws/casinoweb/services/GameManager.java
@@ -4,16 +4,35 @@ import com.casino.grupo4_dws.casinoweb.model.Game;
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 @Service
 public class GameManager {
 
-    List <Game> ListaJuegos;
+    private List <Game> gameList;
+
+    public GameManager() {
+        this.gameList = new ArrayList<Game>();
+    }
 
     @PostConstruct
-    private void InciarJuegos(){
-        System.out.println("A");
+    private void startGames(){
+        Game TirarDado = new Game(1, "Dado", "Tira un dado de seis caras!", "", 16,0,6);
+        addGame(TirarDado);
     }
+
+    public void addGame(Game game){
+        gameList.add(game);
+    }
+
+    public void removeGameId(int id){
+        gameList.removeIf(g -> g.getId() == id);
+    }
+
+    public List<Game> getGameList(){
+        return gameList;
+    }
+
 }
