47c9cae068bebe0428a1547b0c8024f538b7e839
saultj <sauldelasgalaxias@gmail.com>
2025-02-25 11:10:11 +0100

    gamemanager para mostrar juegos

--- src/main/java/com/casino/grupo4_dws/casinoweb/services/GameManager.java
+++ src/main/java/com/casino/grupo4_dws/casinoweb/services/GameManager.java
@@ -19,8 +19,8 @@ public class GameManager {
 
     @PostConstruct
     private void startGames(){
-        Game TirarDado = new Game(1, "Dado", "Tira un dado de seis caras y prueba tu suerte!", "/images/dice.jpg", 16, 0, 6);
-        Game Ruleta = new Game(2, "Ruleta", "Apuesta a tu número favorito y gira la ruleta!", "/images/roulette.jpg", 20, 1, 36);
+        Game TirarDado = new Game(1, "Dado", "Tira un dado de seis caras y prueba tu suerte!", "/images/", 16, 0, 6);
+        Game Ruleta = new Game(2, "Ruleta", "Apuesta a tu número favorito y gira la ruleta!", "/images/Ssegura.jpg", 20, 1, 36);
         Game Blackjack = new Game(3, "Blackjack", "Juega contra el crupier y llega lo más cerca posible a 21!", "/images/blackjack.jpg", 25, 5, 2);
         
         addGame(TirarDado);
