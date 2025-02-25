d6328d85795c1e574aac403984f387992c3b3f88
saultj <sauldelasgalaxias@gmail.com>
2025-02-25 19:23:54 +0100

    ahora desde los juegos se puede acceder a una pagina dinamica individual para ver mas informacion y posteriormente apostar

--- src/main/java/com/casino/grupo4_dws/casinoweb/controllers/indexController.java
+++ src/main/java/com/casino/grupo4_dws/casinoweb/controllers/indexController.java
@@ -1,13 +1,13 @@
 package com.casino.grupo4_dws.casinoweb.controllers;
-
-
 import com.casino.grupo4_dws.casinoweb.model.User;
+import com.casino.grupo4_dws.casinoweb.model.Game;
 import com.casino.grupo4_dws.casinoweb.services.GameManager;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class indexController {
@@ -55,6 +55,21 @@ public class indexController {
         return "slots"; // Página slots.html
     }
 
+    @GetMapping("/game/{id}")
+    public String showGameDetails(@PathVariable int id, Model model, HttpSession session) {
+        Game game = Services.getGame(id);
+        if (game == null) {
+            return "redirect:/NJuegos";
+        }
+        
+        User user = (User) session.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("user", user);
+        }
+        
+        model.addAttribute("game", game);
+        return "game-details";
+    }
 }
 
 
