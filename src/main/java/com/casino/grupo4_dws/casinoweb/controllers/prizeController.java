fdb34bbf569040e96c64b3faca6cd3159595ffc9
Andrés Alonso <andresalonsoperez2605@gmail.com>
2025-02-26 00:27:40 +0100

    Creada la funcionalidad de comprar Prizes. Los comprados ven reflejados en el inventario del usuario correctamente.

--- src/main/java/com/casino/grupo4_dws/casinoweb/controllers/prizeController.java
+++ src/main/java/com/casino/grupo4_dws/casinoweb/controllers/prizeController.java
@@ -11,12 +11,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class prizeController {
     @Autowired
     private final PrizeManager prizeManager;
+
     public prizeController(PrizeManager prizeManager) {
         this.prizeManager = prizeManager;
     }
@@ -26,7 +28,7 @@ public class prizeController {
 
     @GetMapping("/prizes")
     public String showPrizes(Model model, HttpSession session) {
-        List <Prize> prizes = prizeManager.getPrizeList();
+        List<Prize> prizes = prizeManager.getPrizeList();
         model.addAttribute("prizes", prizes);
         User user = (User) session.getAttribute("user");
         if (user == null) {
@@ -41,31 +43,53 @@ public class prizeController {
         model.addAttribute("newPrize", new Prize());
         return "staticLoggedIn/addPrizeForm";
     }
+
     @PostMapping("/addPrize")
     public String addGame(@ModelAttribute("newPrize") Prize newPrize) {
         prizeManager.addPrize(newPrize);
         return "redirect:/prizes";
     }
+
     @PostMapping("/deletePrize/{id}")
     public String eliminarJuego(@PathVariable int id) {
         prizeManager.removePrizeId(id);
         return "redirect:/prizes";
     }
+
     @GetMapping("/editPrize/{id}")
     public String editPrize(Model model, @PathVariable int id) {
         Prize editado = prizeManager.getPrize(id);
         model.addAttribute("prize", editado);
         return "staticLoggedIn/editPrizeForm";
     }
+
     @PostMapping("/updatePrize/{id}")
     public String updatePrize(@ModelAttribute("prize") Prize updatedPrize, @PathVariable int id) {
-        prizeManager.updatePrize(updatedPrize,id);
+        prizeManager.updatePrize(updatedPrize, id);
         return "redirect:/prizes";
     }
 
+    @PostMapping("/buy/{id}")
+    public String buyPrize(@PathVariable int id, Model model, HttpSession session) {
+        User user = (User) session.getAttribute("user");
+        if (user == null) {
+            return "redirect:/login";
+        }
+        Prize prizeBought = prizeManager.getPrize(id);
+        if (prizeBought == null) {
+            return "redirect:/prizes";
+        }
+        if (prizeBought.getPrice() > user.getMoney()) {
+            return "redirect:/prizes";
+        }
+        user.setMoney(user.getMoney() - prizeBought.getPrice());
 
-//    @PostMapping("/buyPrize/{id}")
-//    public String buyPrize(@PathVariable int id) {
-//    }
+        if(user.getInventario() == null){
+            user.setInventario(new ArrayList<>());
+        }
+        user.getInventario().add(prizeBought);
+        model.addAttribute("user", user);
 
+        return "redirect:/prizes";
+    }
 }
