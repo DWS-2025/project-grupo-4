c5720d5d080f4220dbba2e4560b690c50945faf0
RalpiRekt <a.camacho.2023@alumnos.urjc.es>
2025-05-16 21:01:47 +0200

    dto bug

--- src/main/java/com/casino/grupo4_dws/casinoweb/dto/PrizeDTO.java
+++ src/main/java/com/casino/grupo4_dws/casinoweb/dto/PrizeDTO.java
@@ -1,49 +1,14 @@
 package com.casino.grupo4_dws.casinoweb.dto;
 
-public class PrizeDTO {
-    private int id;
-    private String title;
-    private String description;
-    private int price;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
-    public PrizeDTO() {}
+import java.sql.Blob;
 
-    public PrizeDTO(int id, String title, String description, int price) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.price = price;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
+public record PrizeDTO(int id,
+                       String title,
+                       String description,
+                       int price,
+                       int owner,
+                       @JsonIgnore
+                       Blob image) {
 }
